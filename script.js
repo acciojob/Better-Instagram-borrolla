990e16e3a6cfbf020aeaf84c47447e0353bedefd
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-04-02 13:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your code here
+let dragIndex = 0;
+let dropIndex = 0;
+let clone="";
+
+const images = document.querySelectorAll(".image");
+
+function drag(event){
+	event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event){
+	event.preventDefault();
+	clone = event.target.cloneNode(true);
+	let data=event.dataTransfer.getData("text");
+	const parentNode = document.getElementById("parent");
+	let nodelist = parentNode.childNodes;
+
+	nodelist.forEach((list, index) => {
+		if(list.id == data)
+		{
+			dragIndex = index;//storing the dragged element index
+		}
+	});
+//inserting 
+    parentNode.insertBefore(clone, document.getElementById(data));
+    //replacing
+    parentNode.replaceChild(document.getElementById(data), event.target);
+}
+	
+
+function allowDrop(event){
+	event.preventDefault();
+}
+
+const dragdrop = (image) =>{
+	//do something
+	//console.log(image)
+	image.ondragstart = drag;
+	image.ondragover = allowDrop;
+	image.ondrop = drop;
+}
+
+images.forEach(dragdrop);
+
+
+
+
+
